bcbd6d008de3b9bc1fe4bc0fa2e79f68e8024fc6
Laura Gonzalez <laura.gonzmau@gmail.com>
2023-08-10 17:43:06 -0300

    Fixin mistakes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,28 +7,31 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="d-flex flex-column align-items-center header">
+    <div className="header">
       <div className="d-flex justify-content-between align-items-center">
-        <h1 className="me-2 header-title">My Tinerary</h1>
-        <img className="header-logo" src="/localizacion.png" alt="" />
+        <div className="d-flex align-items-center">
+          <img className="header-logo" src="/localizacion.png" alt="" />
+          <h1 className="me-2 header-title">My Tinerary</h1>
+        </div>
+        <div className="d-flex align-items-center">
+          <Navbar expand="md" className="w-auto">
+            <Container className="container-toggle">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
+              <Navbar.Collapse id="basic-navbar-nav custom-basic-nav">
+                <Nav className="ms-auto">
+                  <Nav.Link as={Link} to="/" className="custom-link">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/cities" className="custom-link">
+                    Cities
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          <button className="btn custom-btn"><img className='img-user' src="../../public/user-interface.png" alt="" /></button>
+        </div>
       </div>
-
-      <Navbar expand="md" className="w-100 mb-2">
-        <Container className="container-toggle">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
-          <Navbar.Collapse id="basic-navbar-nav custom-basic-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/" className="custom-link">
-                Home
-              </Nav.Link>
-              <Nav.Link as={Link} to="/cities" className="custom-link">
-                Cities
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <button className="btn custom-btn"><img className='img-user' src="../../public/user-interface.png" alt="" /></button>
     </div>
   );
 }
