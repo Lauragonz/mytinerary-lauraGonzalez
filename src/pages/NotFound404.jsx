37bdffdad0928516edaff441a0d2145ecf67d036
Laura Gonzalez <laura.gonzmau@gmail.com>
2023-08-10 17:52:33 -0300

    Adding styles

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -1,10 +1,15 @@
+import './NotFound.css'
+
 export default function ErrorPage() {
   
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+    <div id="error-page" className="errorPage">
+      <h2 className='Opps'>Oops!</h2>
+
+      <img className='imgNF' src="/404-error.png" alt="" />
+
+      <p className='oops-p'>Sorry, an unexpected error has occurred.</p>
       
     </div>
   );
